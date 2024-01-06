494ca956962cc5a76f1a1d5cb3b70f0f0b6693d0
sunny kumar <138433902+SunnyKumar1709@users.noreply.github.com>
2024-01-06 20:24:56 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="c_header">
+        <p id="header">Home</p>
+        <p id="header">Product</p>
+        <p id="header">Help</p>
+      </div>
+    </header>
+  );
+}
