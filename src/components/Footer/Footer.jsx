8ca017a125a147c6bc313f2b83dab0f5426534fd
sunny kumar <138433902+SunnyKumar1709@users.noreply.github.com>
2024-01-06 20:26:59 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="c_footer">
+        <p id="footer">All rights are reserved - 2024</p>
+      </div>
+    </footer>
+  );
+}
